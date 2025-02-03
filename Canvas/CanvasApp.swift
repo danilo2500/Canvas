47283a1824128cbc19eb5633d810491cf401e51a
Danilo Henrique <danilo.2500@gmail.com>
2025-02-03 17:52:54 -0300

    Add xcode project

---
+++ Canvas/CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasApp.swift
+//  Canvas
+//
+//  Created by Danilo Henrique on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Canvas/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Canvas
+//
+//  Created by Danilo Henrique on 03/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
