0a3e83b51cb9f6530249a620f4d2252666dcc699
Danilo Henrique <danilo.2500@gmail.com>
2025-02-05 21:37:24 -0300

    Add rest service module

---
+++ Canvas/Modules/ RESTService/RESTErrors.swift
@@ -0,0 +1,27 @@
+//
+//  NSErrors.swift
+//  REST-Service
+//
+//  Created by Danilo Henrique on 15/04/24.
+//
+
+import Foundation
+
+enum RESTError: Error {
+    case failedToCreateURL
+    case failedToCreateImage
+}
+
+
+// MARK: - Error Descriptions
+
+extension RESTError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .failedToCreateURL:
+            return "Failed to create URL"
+        case .failedToCreateImage:
+            return "Data provided could not create image"
+        }
+    }
+}
