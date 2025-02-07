7a4dc8bcb865bd2c021c458a255e316be76ea74a
Danilo Henrique <danilo.2500@gmail.com>
2025-02-07 10:59:03 -0300

    Add view model to canvas view

--- Canvas/CanvasApp.swift
+++ Canvas/CanvasApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CanvasApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            CanvasView()
         }
     }
 }
---
+++ Canvas/Screens/Main/CanvasViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  Canvas
+//
+//  Created by Danilo Henrique on 07/02/25.
+//
+
+import SwiftUI
+
+class CanvasViewModel: ObservableObject {
+    
+}
