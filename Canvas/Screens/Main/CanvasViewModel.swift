84863872398b939d3eb5d01b70e57a6944d0d595
Danilo Henrique <danilo.2500@gmail.com>
2025-02-08 14:21:59 -0300

    Add logic to snap horizonatally

--- Canvas/Screens/Main/CanvasViewModel.swift
+++ Canvas/Screens/Main/CanvasViewModel.swift
@@ -13,6 +13,12 @@ struct CanvasImage: Identifiable {
     var position: CGPoint
     var isSelected: Bool = false
     var scale: CGFloat = 1.0
+    var width: CGFloat
+    var height: CGFloat
+    
+    var rect: CGRect {
+        CGRect(origin: position, size: CGSize(width: width * scale, height: height * scale))
+    }
 }
 
 class CanvasViewModel: ObservableObject {
@@ -20,8 +26,8 @@ class CanvasViewModel: ObservableObject {
     // MARK: Properties
     
     @Published var images: [CanvasImage] = [
-        .init(image: UIImage(named: "bee")!, position: .init(x: 100, y: 100)),
-        .init(image: UIImage(named: "bee")!, position: .init(x: 200, y: 100))
+        .init(image:  UIImage(named: "bee")!, position: .init(x: 0, y: 0), width: 50, height: 50),
+        .init(image:  UIImage(named: "bee")!, position: .init(x: 100, y: 100), width: 50, height: 50),
     ]
     
     // MARK: Public Functions
@@ -30,6 +36,7 @@ class CanvasViewModel: ObservableObject {
         if let index = images.firstIndex(where: { $0.id == id }) {
             if images[index].isSelected {
                 images[index].position = location
+                snapToGuidelinesIfNeeded(imageToSnap: images[index])
             }
         }
     }
@@ -53,5 +60,38 @@ class CanvasViewModel: ObservableObject {
         for index in images.indices {
             images[index].isSelected = false
         }
+    }
+    //should snap
+    private func snapToGuidelinesIfNeeded(imageToSnap: CanvasImage) -> CGPoint {
+        guard let index = images.firstIndex(where: { $0.id == imageToSnap.id }) else { return .zero }
+
+        var point = imageToSnap.position
+        let snapSize: CGFloat = 25
+        
+        for image in images where imageToSnap.id != image.id {
+            if image.rect.intersects(imageToSnap.rect) {
+                print("imageToSnap", imageToSnap.rect)
+                print("image", image.rect)
+                
+                
+                let intersection = imageToSnap.rect.intersection(image.rect)
+                print("width", intersection.width)
+                //snap horizontally
+                if intersection.width < snapSize {
+                    if imageToSnap.rect.minX > image.rect.midX { //snap to right
+                        images[index].position.x = image.rect.maxX
+//                        images[index].position = imageToSnap.rect.offsetBy(dx: intersection.width, dy: 0).origin
+                        print("offset", image.rect.offsetBy(dx: intersection.width, dy: 0).origin)
+                    } else {
+                        images[index].position = imageToSnap.rect.offsetBy(dx: -intersection.width, dy: 0).origin
+//                        images[index].position = image.rect.offsetBy(dx: -intersection.minX, dy: 0).origin
+                    }
+                }
+                
+                
+                //snap vertically
+            }
+        }
+        return .zero
     }
 }
